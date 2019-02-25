9aa712bfd3b87d87a3a6f1c0776392a5508d370e
Simon Leier <simonleier@gmail.com>
2019-02-25 17:32:06 +0100

    Fix travis

--- travis/commit.sh
+++ travis/commit.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
+set -e
+
 cd compiler
+pub get
 pub run main
 cd ../
 
 if [ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]
 then
-git config --global user.email "travis@travis-ci.com"
-git config --global user.name "Travis CI"
-git add README.md
-git commit --message "README.md build [ci skip]"
-git push https://${GH_TOKEN}@github.com/leisim/awesome-flutter-packages.git
+  git config --global user.email "travis@travis-ci.com"
+  git config --global user.name "Travis CI"
+  git add README.md
+  git commit --message "README.md build [ci skip]"
+  git push https://${GH_TOKEN}@github.com/leisim/awesome-flutter-packages.git
 fi
